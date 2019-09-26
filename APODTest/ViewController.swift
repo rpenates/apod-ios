ff736d4a89e80223b6de0e2fb32590925a5c0548
Rafael Peñates <andres.penates@gmail.com>
2019-09-26 15:50:30 -0500

    Add APOD logic

--- APODTest/ViewController.swift
+++ APODTest/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import AlamofireImage
 
 class ViewController: UIViewController {
+    @IBOutlet weak var apodTitle: UILabel!
+    @IBOutlet weak var apodImage: UIImageView!
+    @IBOutlet weak var apodDescription: UITextView!
+    
+    let apiUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationItem.title = "APOD Test"
+        
+        getApod()
+    }
+    
+    func getApod() {
+        Alamofire.request(apiUrl, method: .get)
+            .responseJSON { response in
+                let jsonApod = JSON(response.data!)
+                
+                self.apodTitle.text = jsonApod["title"].stringValue
+                Alamofire.request(jsonApod["url"].stringValue, method: .get)
+                    .responseImage { response in
+                        if (response.result.value != nil) {
+                            let fetchedImage = UIImage(data: response.data!, scale: 1.0)
+                            self.apodImage.image = fetchedImage
+                        } else {
+                            print ("Error loading image")
+                        }
+                }
+                self.apodDescription.text = jsonApod["explanation"].stringValue
+                
+        }
     }
 
 
